a59cb2e15edb0aa20e010933263d3b8de6835bcd
Erik <erik.ondra@yahoo.com>
2024-07-20 22:52:47 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html
+            lang="en"
+            suppressHydrationWarning>
+            <head />
+            <body className={cn("bg-background font-sans antialiased", fontSans.variable)}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange>
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
---
+++ src/components/ui/headline.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type HeadlineProps = {
+    as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+    className?: string;
+    children: React.ReactNode;
+};
+
+const Headline = ({ as: Tag = "h1", className = "", children }: HeadlineProps) => {
+    return <Tag className={`text-3xl font-bold md:text-4xl lg:text-5xl ${className}`}>{children}</Tag>;
+};
+
+export default Headline;
