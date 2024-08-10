d6342d25b4e3b05db024220dbff2f745777503fd
Erik <erik.ondra@yahoo.com>
2024-08-10 09:05:50 +0200

    added animations for OccasionSelector.tsx

---
+++ src/components/OccasionSelector.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const OccasionSelector = () => {
+	let musicOccasions = [
+		"Working Out",
+		"Partying",
+		"Waking Up",
+		"Studying",
+		"Cooking",
+		"Meditating",
+		"Driving",
+		"Gaming",
+		"Traveling",
+		"Chilling",
+		"Sleeping",
+	];
+
+	return <div></div>;
+};
+
+export default OccasionSelector;
